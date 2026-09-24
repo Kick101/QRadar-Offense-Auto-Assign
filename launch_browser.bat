9235ff7970f4058265434f02e75f308197bd53f7
Kick101 <46389158+Kick101@users.noreply.github.com>
2026-09-24 19:13:13 +0530

    init
    initial commit

---
+++ launch_browser.bat
@@ -0,0 +1,3 @@
+REM Paste the full path to the Chromium-based browser executable below
+
+"C:\Users\user\AppData\Local\BraveSoftware\Brave-Browser\Application\brave.exe" --remote-debugging-port=9222 --user-data-dir="C:\temp\brave-playwright"
